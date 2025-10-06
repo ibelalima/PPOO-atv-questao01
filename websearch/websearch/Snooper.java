c3e7667c5f8083e20c86a2b516945b44d765c10a
Isabela <isabela.franca.lima@gmail.com>
2025-10-06 19:49:34 -0300

    Criando dois observadores com filtros diferentes no Snooper (friend e long lines)

--- websearch/websearch/Snooper.java
+++ websearch/websearch/Snooper.java
@@ -6,12 +6,39 @@ public class Snooper {
 
     public Snooper(WebSearchModel model) {
         this.model = model;
+    WebSearchModel.QueryFilter friendFilter = new WebSearchModel.QueryFilter() {
+            @Override
+            public boolean shouldNotify(String query) {
+                return query.toLowerCase().contains("friend");
+            }
+        };
 
-        model.addQueryObserver( new WebSearchModel.QueryObserver() {
+        // Observador para consultas com "friend"
+        WebSearchModel.QueryObserver friendObserver = new WebSearchModel.QueryObserver() {
             @Override
             public void onQuery(String query) {
-                System.out.println("Query: " + query);
+                System.out.println("Oh Yes! " + query);
             }
-        });
+        };
+
+        // Filtro que aceita consultas com mais de 60 caracteres
+        WebSearchModel.QueryFilter longFilter = new WebSearchModel.QueryFilter() {
+            @Override
+            public boolean shouldNotify(String query) {
+                return query.length() > 60;
+            }
+        };
+
+        // Observador para consultas longas
+        WebSearchModel.QueryObserver longObserver = new WebSearchModel.QueryObserver() {
+            @Override
+            public void onQuery(String query) {
+                System.out.println("So long " + query);
+            }
+        };
+
+        // Registra ambos os observadores com seus filtros (strategies)
+        model.addQueryObserver(friendObserver, friendFilter);
+        model.addQueryObserver(longObserver, longFilter);
     }
 }
